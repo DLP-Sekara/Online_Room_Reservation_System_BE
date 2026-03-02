9c61d843b2e533be4ac1c1eaa0a54bccc0fe01cf
lahirupiyumal <dslahirupiyumal99@gmail.com>
2026-03-02 21:55:53 +0530

    Dev: Added main configurations

---
+++ src/main/java/com/example/ov_artifact/OvArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.example.ov_artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvArtifactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ov_artifact/OvArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ov_artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
